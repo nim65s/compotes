8d741932521a421ed231c84b4310b3300b8f783b
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-01-31 18:10:16 +0100

    init django

---
+++ compotes/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for compotes project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "compotes.settings")
+
+application = get_wsgi_application()
