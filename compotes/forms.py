b6ee47fc2e48ca8bb76a6645af250fca994d1449
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-03-13 10:40:42 +0100

    add missing description to debtpartsformset

--- compotes/forms.py
+++ compotes/forms.py
@@ -4,4 +4,6 @@ from django.forms.models import inlineformset_factory
 
 from .models import Debt, Part
 
-DebtPartsFormset = inlineformset_factory(Debt, Part, fields=["debitor", "part"])
+DebtPartsFormset = inlineformset_factory(
+    Debt, Part, fields=["debitor", "part", "description"]
+)
