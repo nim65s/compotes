ea95a98cfcf9f055f46d2e4d5725c535eeb2a214
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-08-29 22:50:36 +0200

    debt filter, fix #15

--- compotes/filters.py
+++ compotes/filters.py
@@ -12,16 +12,27 @@ class DebtFilter(django_filters.FilterSet):
     """FilterSet for the Debt model."""
 
     user = django_filters.CharFilter(label=_("User"), method="user_filter")
+    debt = django_filters.CharFilter(label=_("Debt"), method="debt_filter")
 
     class Meta:
         """Meta."""
 
         model = Debt
-        fields = ["user"]
+        fields = ["user", "debt"]
 
     def user_filter(self, queryset, name, value):
-        """Filter debt by username."""
+        """Filter debts by creditor / debitor username."""
         return queryset.filter(
             Q(creditor__username__icontains=value)
+            | Q(creditor__last_name__icontains=value)
+            | Q(creditor__first_name__icontains=value)
             | Q(part__debitor__username__icontains=value)
+            | Q(part__debitor__last_name__icontains=value)
+            | Q(part__debitor__first_name__icontains=value)
         ).distinct()
+
+    def debt_filter(self, queryset, nam, value):
+        """Filter debts by name / description."""
+        return queryset.filter(
+            Q(name__icontains=value) | Q(description__icontains=value)
+        )
