2f07129a27495255141d44d2c72c124ee654b842
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-08-30 11:10:23 +0200

    add balance in UserAdmin

--- compotes/admin.py
+++ compotes/admin.py
@@ -1,11 +1,19 @@
 """Compotes admin ui."""
 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from . import models
 
+
+class UserAdmin(BaseUserAdmin):
+    """Add balance to UserAdmin.list_display."""
+
+    list_display = list(BaseUserAdmin.list_display) + ["balance"]
+
+
 for model in (models.Debt, models.Part, models.Pool, models.Share):
     admin.site.register(model)
+
 
 admin.site.register(models.User, UserAdmin)
