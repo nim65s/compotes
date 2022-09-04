f7f056e23256940823d4630e29886cbec4cba9dc
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-09-04 16:49:51 +0200

    add update-balance admin-link, fix #24

--- compotes/admin.py
+++ compotes/admin.py
@@ -2,6 +2,8 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.http import HttpResponseRedirect
+from django.urls import path, reverse
 
 from . import models
 
@@ -10,6 +12,18 @@ class UserAdmin(BaseUserAdmin):
     """Add balance to UserAdmin.list_display."""
 
     list_display = list(BaseUserAdmin.list_display) + ["balance"]
+
+    def get_urls(self):
+        """Add update_balance url."""
+        return [
+            path("update-balance", self.admin_site.admin_view(self.update_balance))
+        ] + super().get_urls()
+
+    def update_balance(self, request):
+        """View to update balances."""
+        for user in models.User.objects.all():
+            user.save()
+        return HttpResponseRedirect(reverse("admin:compotes_user_changelist"))
 
 
 for model in (models.Debt, models.Part, models.Pool, models.Share):
