02940cf04ba32b7c2182a8e94e2dcc589d5c0416
Maximilien Naveau <maximilien.naveau@gmail.com>
2026-07-04 09:55:32 +0200

    Add events

--- compotes/views.py
+++ compotes/views.py
@@ -1,23 +1,26 @@
 """Compotes views."""
 
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q, QuerySet
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.utils.translation import gettext_lazy as _
+from django.views import View
 from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
+from django.views.generic.detail import SingleObjectMixin
 
 from django_filters.views import FilterView
 from django_tables2 import SingleTableMixin, SingleTableView  # type: ignore
 from ndh.mixins import NDHDeleteMixin, NDHFormMixin
 
-from actions.models import Action
+from actions.models import Action, to_json
 from actions.views import ActionCreateMixin, ActionDeleteMixin, ActionUpdateMixin
 
 from .filters import DebtFilter
-from .forms import DebtForm, PartForm, ShareForm
-from .models import Debt, Part, Pool, Share, User
-from .tables import DebtTable, PoolTable, UserTable
+from .forms import DebtForm, EventForm, PartForm, ShareForm
+from .models import Debt, Event, Part, Pool, Share, User
+from .tables import DebtTable, EventTable, PoolTable, UserTable
 
 
 class UserListView(LoginRequiredMixin, SingleTableView):
@@ -46,6 +49,110 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     """Debt detail view."""
 
     model = User
+
+
+class EventListView(LoginRequiredMixin, SingleTableView):
+    """Event list view."""
+
+    model = Event
+    table_class = EventTable
+
+    def get_queryset(self) -> QuerySet:
+        """Show only events the user organises or has debts in."""
+        user = self.request.user
+        return self.model.objects.filter(
+            Q(organiser=user) | Q(debt__creditor=user) | Q(debt__part__debitor=user),
+        ).distinct()
+
+
+class EventCreateView(LoginRequiredMixin, NDHFormMixin, ActionCreateMixin, CreateView):
+    """Event create view."""
+
+    model = Event
+    form_class = EventForm
+    title = _("Add an Event")
+
+    def form_valid(self, form) -> HttpResponse:
+        """Document organiser."""
+        form.instance.organiser = self.request.user
+        return super().form_valid(form)
+
+
+class EventDetailView(LoginRequiredMixin, DetailView):
+    """Event detail view."""
+
+    model = Event
+
+    def get_context_data(self, **kwargs):
+        """Add per-participant balances and related actions."""
+        balances = [
+            (user, user.get_event_balance(self.object))
+            for user in self.object.participants()
+        ]
+        actions = Action.objects.filter(
+            Q(json__model="compotes.event", json__pk=self.object.pk)
+            | Q(json__model="compotes.debt", json__fields__event=self.object.pk),
+        )
+        return super().get_context_data(
+            balances=balances,
+            unsettled=any(balance for _user, balance in balances),
+            actions=actions,
+            **kwargs,
+        )
+
+
+class EventUpdateView(LoginRequiredMixin, NDHFormMixin, ActionUpdateMixin, UpdateView):
+    """Event update view."""
+
+    model = Event
+    form_class = EventForm
+    title = _("Edit an event")
+
+
+class EventCloseView(LoginRequiredMixin, SingleObjectMixin, View):
+    """Close an Event, requiring confirmation if balances aren't settled."""
+
+    model = Event
+
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        """Fold leftover balances into the global pool, once confirmed."""
+        event = self.get_object()
+        unsettled = [
+            user for user in event.participants() if user.get_event_balance(event) != 0
+        ]
+        if unsettled and not request.POST.get("confirm"):
+            messages.warning(
+                request,
+                _(
+                    "Some balances are not settled yet: check the "
+                    "confirmation box to close anyway.",
+                ),
+            )
+            return redirect(event.get_absolute_url())
+        event.close()
+        Action.objects.create(
+            user=request.user,
+            act=Action.Act.UPDATE,
+            json=to_json(event),
+        )
+        return redirect(event.get_absolute_url())
+
+
+class EventReopenView(LoginRequiredMixin, SingleObjectMixin, View):
+    """Reopen a closed Event, isolating its Debts from the global balance again."""
+
+    model = Event
+
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        """Reopen the Event."""
+        event = self.get_object()
+        event.reopen()
+        Action.objects.create(
+            user=request.user,
+            act=Action.Act.UPDATE,
+            json=to_json(event),
+        )
+        return redirect(event.get_absolute_url())
 
 
 class DebtListView(LoginRequiredMixin, SingleTableMixin, FilterView):
